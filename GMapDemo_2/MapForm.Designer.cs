3c8c19dd95e42c03f67b39631d1c46b7eac283e7
dugking <bingwei2642@qq.com>
2018-01-31 12:31:53 +0800

    截图

--- GMapDemo_2/MapForm.Designer.cs
+++ GMapDemo_2/MapForm.Designer.cs
@@ -37,10 +37,23 @@
             this.GMapMean = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.CreatePoint = new System.Windows.Forms.ToolStripMenuItem();
             this.新建路线ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.mapControl = new GMapDemo_2.MapControl();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.保存地图ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.select = new System.Windows.Forms.Button();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mapControl = new GMapDemo_2.MapControl();
+            this.checkBoxDrawPolygon = new System.Windows.Forms.RadioButton();
+            this.drawYuan = new System.Windows.Forms.RadioButton();
             this.statusWin.SuspendLayout();
             this.GMapMean.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // statusWin
@@ -84,9 +97,10 @@
             // 
             this.GMapMean.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.CreatePoint,
-            this.新建路线ToolStripMenuItem});
+            this.新建路线ToolStripMenuItem,
+            this.保存地图ToolStripMenuItem});
             this.GMapMean.Name = "GMapMean";
-            this.GMapMean.Size = new System.Drawing.Size(125, 48);
+            this.GMapMean.Size = new System.Drawing.Size(125, 70);
             // 
             // CreatePoint
             // 
@@ -101,6 +115,107 @@
             this.新建路线ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
             this.新建路线ToolStripMenuItem.Text = "新建路线";
             this.新建路线ToolStripMenuItem.Click += new System.EventHandler(this.新建路线ToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(6, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(135, 48);
+            this.panel1.TabIndex = 2;
+            // 
+            // 保存地图ToolStripMenuItem
+            // 
+            this.保存地图ToolStripMenuItem.Name = "保存地图ToolStripMenuItem";
+            this.保存地图ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.保存地图ToolStripMenuItem.Text = "保存地图";
+            this.保存地图ToolStripMenuItem.Click += new System.EventHandler(this.保存地图ToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(27, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "+";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(93, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 14);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "-";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.drawYuan);
+            this.panel2.Controls.Add(this.checkBoxDrawPolygon);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.txtCity);
+            this.panel2.Controls.Add(this.select);
+            this.panel2.Controls.Add(this.txtAddress);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(733, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(150, 551);
+            this.panel2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "地址:";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(47, 103);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(100, 21);
+            this.txtAddress.TabIndex = 4;
+            // 
+            // select
+            // 
+            this.select.Location = new System.Drawing.Point(97, 139);
+            this.select.Name = "select";
+            this.select.Size = new System.Drawing.Size(49, 20);
+            this.select.TabIndex = 5;
+            this.select.Text = "查找";
+            this.select.UseVisualStyleBackColor = true;
+            this.select.Click += new System.EventHandler(this.select_Click);
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(47, 76);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 21);
+            this.txtCity.TabIndex = 6;
+            this.txtCity.Text = "昆明";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "城市:";
             // 
             // mapControl
             // 
@@ -129,30 +244,54 @@
             this.mapControl.Size = new System.Drawing.Size(883, 573);
             this.mapControl.TabIndex = 0;
             this.mapControl.Zoom = 0D;
+            this.mapControl.OnPolygonEnter += new GMap.NET.WindowsForms.PolygonEnter(this.mapControl_OnPolygonEnter);
+            this.mapControl.OnPolygonLeave += new GMap.NET.WindowsForms.PolygonLeave(this.mapControl_OnPolygonLeave);
+            this.mapControl.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.mapControl_MouseDoubleClick);
+            this.mapControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mapControl_MouseDown);
+            this.mapControl.MouseEnter += new System.EventHandler(this.mapControl_MouseEnter);
             this.mapControl.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mapControl_MouseMove);
             // 
-            // panel1
+            // checkBoxDrawPolygon
             // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(757, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(126, 551);
-            this.panel1.TabIndex = 2;
+            this.checkBoxDrawPolygon.AutoSize = true;
+            this.checkBoxDrawPolygon.Location = new System.Drawing.Point(12, 179);
+            this.checkBoxDrawPolygon.Name = "checkBoxDrawPolygon";
+            this.checkBoxDrawPolygon.Size = new System.Drawing.Size(59, 16);
+            this.checkBoxDrawPolygon.TabIndex = 8;
+            this.checkBoxDrawPolygon.TabStop = true;
+            this.checkBoxDrawPolygon.Text = "多边形";
+            this.checkBoxDrawPolygon.UseVisualStyleBackColor = true;
+            // 
+            // drawYuan
+            // 
+            this.drawYuan.AutoSize = true;
+            this.drawYuan.Location = new System.Drawing.Point(82, 179);
+            this.drawYuan.Name = "drawYuan";
+            this.drawYuan.Size = new System.Drawing.Size(47, 16);
+            this.drawYuan.TabIndex = 8;
+            this.drawYuan.TabStop = true;
+            this.drawYuan.Text = "圆形";
+            this.drawYuan.UseVisualStyleBackColor = true;
             // 
             // MapForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(883, 573);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.statusWin);
             this.Controls.Add(this.mapControl);
             this.Name = "MapForm";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.SizeChanged += new System.EventHandler(this.MapForm_SizeChanged);
             this.statusWin.ResumeLayout(false);
             this.statusWin.PerformLayout();
             this.GMapMean.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -170,6 +309,17 @@
         private System.Windows.Forms.ToolStripMenuItem CreatePoint;
         private System.Windows.Forms.ToolStripMenuItem 新建路线ToolStripMenuItem;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem 保存地图ToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button select;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.RadioButton checkBoxDrawPolygon;
+        private System.Windows.Forms.RadioButton drawYuan;
     }
 }
 
--- GMapDemo_2/MapForm.cs
+++ GMapDemo_2/MapForm.cs
@@ -16,9 +16,18 @@ namespace GMapDemo_2
     public partial class MapForm : Form
     {
         private GMapOverlay markersOverlay = new GMapOverlay("markers"); //放置marker的图层
+        private GMapOverlay objects; //放置marker的图层
+        private GMapOverlay routes;
+        private GMapOverlay polygons = new GMapOverlay("polygon"); //放置polygon的图层
+        private GMapPolygon drawingPolygon = null; //正在画的polygon
+        private List<PointLatLng> drawingPoints = new List<PointLatLng>(); //多边形的点集
+
         public static Dictionary<string, PointLatLng> dict = new Dictionary<string, PointLatLng>();
         public static Dictionary<string, string> dictPlace = new Dictionary<string, string>();
         private static int count = 1;
+        private GMapPolygon currentPolygon;
+
+        private bool isLeftButtonDown = false;
         public MapForm()
         {
             InitializeComponent();
@@ -41,11 +50,18 @@ namespace GMapDemo_2
             mapControl.ShowCenter = false; //不显示中心十字点
             mapControl.DragButton = System.Windows.Forms.MouseButtons.Left; //左键拖拽地图
 
-            var point = MapControl.MousePosition;
 
 
-            mapControl.Position = new PointLatLng(45.74740199642105, 126.69570922851562);
 
+            //mapControl.Position = new PointLatLng(45.74740199642105, 126.69570922851562);
+            mapControl.SetPositionByKeywords("china,kunming");//设置初始中心为china harbin
+
+
+            this.routes = new GMapOverlay("routes");
+            this.mapControl.Overlays.Add(this.routes);
+            this.objects = new GMapOverlay("objects");
+            this.mapControl.Overlays.Add(this.objects);
+            this.mapControl.Overlays.Add(this.polygons);
             mapControl.Overlays.Add(markersOverlay);
 
 
@@ -64,17 +80,21 @@ namespace GMapDemo_2
         private void CreatePoint_Click(object sender, EventArgs e)
         {
 
-            addPoint add=new addPoint();
+            addPoint add = new addPoint();
             add.ShowDialog();
             PointLatLng point = mapControl.FromLocalToLatLng(Convert.ToInt32(latText.Text), Convert.ToInt32(lngText.Text));
+
+
             
+
             GMapMarker marker = new GMarkerGoogle(point, GMarkerGoogleType.green);
             marker.ToolTipText = add.text;
             marker.ToolTipMode = MarkerTooltipMode.Always;
             markersOverlay.Markers.Add(marker);
             dict.Add(Convert.ToString(count), point);
-            dictPlace.Add(Convert.ToString(count),add.text);
+            dictPlace.Add(Convert.ToString(count), add.text);
             count += 1;
+            add.Dispose();
         }
 
         private void DrawrouteBetweenTowPoint(PointLatLng pointLatLng_S, PointLatLng pointLatLng_E)
@@ -89,18 +109,18 @@ namespace GMapDemo_2
             {
                 GMapRoute r = new GMapRoute(route.Points, route.Name);
                 markersOverlay.Routes.Add(r);
-                 mapControl.ZoomAndCenterRoute(r);
+                mapControl.ZoomAndCenterRoute(r);
             }
         }
 
         private void 新建路线ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-           
+
             addPointRoute add = new addPointRoute();
             add.ShowDialog();
             PointLatLng pointLatLng_S = dict[add.start1];
             PointLatLng pointLatLng_E = dict[add.end2];
-            DrawrouteBetweenTowPoint(pointLatLng_S, pointLatLng_E,add.c);
+            DrawrouteBetweenTowPoint(pointLatLng_S, pointLatLng_E, add.c);
             add.Dispose();
 
         }
@@ -118,9 +138,156 @@ namespace GMapDemo_2
                 GMapRoute r = new GMapRoute(route.Points, route.Name);
                 //r.Stroke.Width = 3;                  //路径宽度  
                 r.Stroke.Color = c;          //路径颜色  
+
                 markersOverlay.Routes.Add(r);
+
                 mapControl.ZoomAndCenterRoute(r);
             }
+        }
+
+        private void 保存地图ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SaveFileDialog dialog = new SaveFileDialog())
+                {
+                    dialog.Filter = "PNG (*.png)|*.png";
+                    dialog.FileName = "GMap.NET image";
+                    Image image = this.mapControl.ToImage();
+                    if (image != null)
+                    {
+                        using (image)
+                        {
+                            if (dialog.ShowDialog() == DialogResult.OK)
+                            {
+                                string fileName = dialog.FileName;
+                                if (!fileName.EndsWith(".png", StringComparison.OrdinalIgnoreCase))
+                                {
+                                    fileName += ".png";
+                                }
+                                image.Save(fileName);
+                                MessageBox.Show("图片已保存： " + dialog.FileName, "GMap.NET", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("图片保存失败： " + exception.Message, "GMap.NET", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            mapControl.Zoom += 1;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            mapControl.Zoom -= 1;
+
+        }
+
+        private void MapForm_SizeChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show(panel1.Location.ToString());
+            //panel1.Location = new Point(mapControl.Width - 80,10);
+        }
+
+        private void select_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("暂时无法使用。。。。。");
+            return;
+
+            if (this.txtAddress.Text.Length == 0)
+            {
+                this.txtAddress.Focus();
+                MessageBox.Show("请输入查询的地址");
+            }
+
+            string search = string.Format("{0},{1}", this.txtCity.Text, this.txtAddress.Text);
+            GeoCoderStatusCode code = this.mapControl.SetPositionByKeywords(search);
+
+            if (code != GeoCoderStatusCode.G_GEO_SUCCESS)
+            {
+                MessageBox.Show("地址没有找到：'" + this.txtAddress.Text + "', 原因：" + code.ToString(), "GMap.NET", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            this.objects.Markers.Clear();
+            AddLocation(this.txtAddress.Text);
+        }
+
+        private void AddLocation(string place)
+        {
+
+        }
+
+        private void mapControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                isLeftButtonDown = true;
+
+                if (checkBoxDrawPolygon.Checked)
+                {
+                    drawingPoints.Add(mapControl.FromLocalToLatLng(e.X, e.Y));
+                    if (drawingPolygon == null)
+                    {
+                        
+                        drawingPolygon = new GMapPolygon(drawingPoints, "my polygon");
+                        drawingPolygon.Fill = new SolidBrush(Color.FromArgb(50, Color.Red));
+                        drawingPolygon.Stroke = new Pen(Color.Blue, 2);
+                        drawingPolygon.IsHitTestVisible = true;
+                        polygons.Polygons.Add(drawingPolygon);
+                    }
+                    else
+                    {
+                        drawingPolygon.Points.Clear();
+                        drawingPolygon.Points.AddRange(drawingPoints);
+                        if (polygons.Polygons.Count == 0)
+                        {
+                            polygons.Polygons.Add(drawingPolygon);
+                        }
+                        else
+                        {
+                            mapControl.UpdatePolygonLocalPosition(drawingPolygon);
+                        }
+                    }
+                    mapControl.Refresh();
+                }
+                else if (drawYuan.Checked)
+                {
+                    GMapMarkerCircle
+                }
+            }
+        }
+
+        private void mapControl_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (checkBoxDrawPolygon.Checked && drawingPolygon != null)
+            {
+                polygons.Polygons.Add(drawingPolygon);
+                drawingPolygon = null;
+                drawingPoints.Clear();
+            }
+        }
+
+        private void mapControl_OnPolygonLeave(GMapPolygon item)
+        {
+            currentPolygon = null;
+            item.Stroke.Color = Color.MidnightBlue;
+        }
+
+        private void mapControl_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mapControl_OnPolygonEnter(GMapPolygon item)
+        {
+            currentPolygon = item;
+            item.Stroke.Color = Color.Red;
         }
     }
 }
